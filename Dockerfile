3b8350ce06f98145b2035bdc98533aaefdb2d055
Brad Rush <brad@fyvm.org>
2019-06-06 08:39:11 -0700

    Add actual bot stuff

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ RUN cpanm BPS/HTTP-Server-Simple-0.52.tar.gz
 
 ADD app /app
 
-CMD ["/usr/local/bin/perl", "/app/test-server.pl"]
+CMD ["/usr/local/bin/perl", "/app/reva-server.pl"]
---
+++ app/reva-api.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+{
+package RevaApi;
+ 
+use HTTP::Server::Simple::CGI;
+use base qw(HTTP::Server::Simple::CGI);
+ 
+my %dispatch = (
+    '/api/v1/reply' => \&resp_reply,
+    '/api/v1/reply' => \&resp_train,
+    '/api/v1/stats' => \&resp_stats
+);
+ 
+sub handle_request {
+    my $self = shift;
+    my $cgi  = shift;
+   
+    my $path = $cgi->path_info();
+    my $handler = $dispatch{$path};
+ 
+    if (ref($handler) eq "CODE") {
+        print "HTTP/1.0 200 OK\r\n";
+        $handler->($cgi);
+         
+    } else {
+        print "HTTP/1.0 404 Not found\r\n";
+        print $cgi->header,
+              $cgi->start_html('Not found'),
+              $cgi->h1('Not found'),
+              $cgi->end_html;
+    }
+}
+ 
+sub resp_reply {
+    my $cgi  = shift;   # CGI.pm object
+    return if !ref $cgi;
+     
+    my $query_string = $cgi->param('q');
+
+    my $bot_response = `/usr/local/bin/hailo --brain /app/db/reva.sqlite -r $query_string`
+     
+    print $cgi->header,
+          $cgi->start_html("Hello"),
+          $cgi->h1("$bot_response"),
+          $cgi->end_html;
+}
+
+sub resp_train {
+  # body...
+}
+
+sub resp_stats {
+  # body...
+}
+ 
+} 
+ 
+# start the server on port 3000
+my $pid = RevaApi->new(3000)->background();
+print "Use 'kill $pid' to stop server.\n";
